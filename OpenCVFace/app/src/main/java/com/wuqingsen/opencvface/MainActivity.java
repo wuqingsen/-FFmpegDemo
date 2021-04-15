fd430032fad9530c86f9eb4c41e8aa8e9974ca34
wuqingsen <1243411677@qq.com>
2021-04-15 22:43:09 +0800

    注释添加

--- OpenCVFace/app/src/main/java/com/wuqingsen/opencvface/MainActivity.java
+++ OpenCVFace/app/src/main/java/com/wuqingsen/opencvface/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
+//wqs
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
